94de916f8d782ba04fc98a1f5237bd2ce6c55433
Roshan <lamichhaner40@gmail.com>
2020-12-26 18:35:15 -0500

    Updated SomeStuff, published to pypi :package: :fire:

---
+++ build/lib/fastmath/lib.py
@@ -0,0 +1,14 @@
+import math 
+
+# quadratic formula: (-b + or - sqrt(b^2 - 4ac)) / 2a
+# define constant
+# output is limited to 2 decimal places, can be changed below
+dp = '%.2f'
+
+def qudratic(num1, num2, num3):
+    discRoot = math.sqrt((num2 * num2) - 4 * num1 * num3)
+    root1 = (-num2 + discRoot) / (2 * num1)
+    root2 = (num2 - discRoot) / (2 * num1)
+    return dp % root1, dp % root2
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastmath", # Replace with your own username
+    version="0.0.1",
+    author="Roshan Lamichhane",
+    author_email="roshanlamichhanenepali@gmail.com",
+    description="A math libary for easier use of math formulas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roshanlam/",
+    packages=setuptools.find_packages(),
+    keywords = ['ROSHAN LAMICHHANE', 'ROSHAN', 'FAST MATH', 'FASTMATH'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
